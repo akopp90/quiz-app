6faad4da649aa17b550c764947f84b1923c7c0f7
Andreas Kopp <andreas.kopp90@gmail.com>
2024-09-10 13:59:10 +0200

    minor fixes to the bookmarks

--- quiz.js
+++ quiz.js
@@ -9,25 +9,29 @@ function showAnswer(id) {
 
 let like_flag = false;
 let dislike_flag = false;
-function bookmark(event) {
-  let counter = parseFloat(document.getElementById("counter").innerHTML);
+function bookmark(event, id) {
+  let counter = parseFloat(document.getElementById("counter-" + id).innerHTML);
   var button = event.target.innerText;
   switch (button) {
-    case "like":
+    case "bookmark":
       if (like_flag == false && dislike_flag == false) {
         counter++;
+        document.getElementById("like-" + id).classList.add("active");
+        document.getElementById("counter-" + id).innerHTML = counter;
         like_flag = true;
       } else if (like_flag == false && dislike_flag == true) {
-        counter = counter + 2;
+        counter = counter + 1; //changed this to 1 instead of 2
+        document.getElementById("like-" + id).classList.add("active");
+        document.getElementById("counter-" + id).innerHTML = counter;
         like_flag = true;
         dislike_flag = false;
       } else {
         counter--;
+        document.getElementById("like-" + id).classList.remove("active");
+        document.getElementById("counter-" + id).innerHTML = counter;
         like_flag = false;
       }
       break;
   }
   console.log("the button " + button + " was pressed");
-
-  document.getElementById("counter").innerHTML = counter;
 }
