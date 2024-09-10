cb3ecdd4f82c0445b358b38c8373f5c5b62f99d3
Andreas Kopp <andreas.kopp90@gmail.com>
2024-09-10 10:43:43 +0200

    added counter styles and bookmark

--- quiz.js
+++ quiz.js
@@ -6,3 +6,28 @@ function showAnswer(id) {
     x.style.display = "none";
   }
 }
+
+let like_flag = false;
+let dislike_flag = false;
+function bookmark(event) {
+  let counter = parseFloat(document.getElementById("counter").innerHTML);
+  var button = event.target.innerText;
+  switch (button) {
+    case "like":
+      if (like_flag == false && dislike_flag == false) {
+        counter++;
+        like_flag = true;
+      } else if (like_flag == false && dislike_flag == true) {
+        counter = counter + 2;
+        like_flag = true;
+        dislike_flag = false;
+      } else {
+        counter--;
+        like_flag = false;
+      }
+      break;
+  }
+  console.log("the button " + button + " was pressed");
+
+  document.getElementById("counter").innerHTML = counter;
+}
