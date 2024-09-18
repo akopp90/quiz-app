7cd0c1b34228ed7faae361d7b79c4882b6d11b27
Andreas Kopp <andreas.kopp90@gmail.com>
2024-09-18 12:01:05 +0200

    challange solved

--- quiz.js
+++ quiz.js
@@ -49,3 +49,77 @@ function showBookmarked() {
 bookmarkButton.addEventListener("click", function () {
   showBookmarked();
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+const createButton = document.querySelector('[data-js="create"]');
+const form = document.querySelector('[data-js="form"]');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  const question = data.question;
+  const answer = data.answer;
+  const tag = data.tag;
+  const qId = 1;
+  createQuestion(question, answer, tag, qId);
+});
+const newQuestion = form.elem;
+function createQuestion(question, answer, tag, qId) {
+  const newQuestion = document.createElement("section");
+  newQuestion.classList.add("card");
+  newQuestion.innerHTML = `<h2 class="card__heading">heading</h2>
+        <section class="counter__box">
+          <button
+            class="card__bookmark material-icons"
+            id="like-${qId}"
+            onclick="bookmark(event, ${qId})"
+          >
+            bookmark
+          </button>
+
+          <span id="counter-${qId}" class="counter">0</span>
+        </section>
+        <p class="card__text">
+        ${question}
+        </p>
+        <button
+          class="btn center"
+          onclick="showAnswer(${qId})"
+          id="${qId}"
+          data-js="answer-button"
+        >
+          Show answer
+        </button>
+        <article class="answerbox" name="answer1" id="a3">
+          <p>${answer}</p>
+        </article>
+        <ul class="taglist">
+          <li class="tag">${tag}</li>
+        </ul>`;
+  document.querySelector('[data-js="main"]').append(newQuestion);
+}
