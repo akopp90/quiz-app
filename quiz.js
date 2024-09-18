e7fc675878a76f457c357ee0b5018dc127a61767
Andreas Kopp <andreas.kopp90@gmail.com>
2024-09-18 15:42:43 +0200

    changet button textcontext

--- quiz.js
+++ quiz.js
@@ -5,9 +5,21 @@ answerButton.addEventListener("click", function () {
   showAnswer(answerId);
 });
 function showAnswer(answerId) {
-  var x = document.getElementById("a" + answerId);
+  const x = document.getElementById("a" + answerId);
   x.classList.toggle("answerbox--active");
+
+  // Find the button element that triggered the event
+  const button = document.getElementById(answerId);
+  console.log(button);
+  if (x.classList.contains("answerbox--active")) {
+    console.log(button.textContent);
+    button.textContent = "Hide answer";
+  } else {
+    console.log(button.textContent);
+    button.textContent = "Show answer";
+  }
 }
+
 function getLikeFlag() {}
 let like_flag = [false, false, false];
 let dislike_flag = false;
